7899c37407e7847ea35e5a380ea34a86ad9a7975
BigBossJacob <47572661+BigBossJacob@users.noreply.github.com>
2019-02-26 12:37:02 -0500

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,15 +1,13 @@
 -----------------------------------------------------------------------------------------
 --
--- how to add text to the screen
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- This is how to add a background
 -- 
 -----------------------------------------------------------------------------------------
 
-local myText = display.newText( "Jacob", 160, 100, native.systemFont, 16 )
-myText:setFillColor( 0, 0, 255 )
------------------------------------------------------------------------
-display.setDefault( "background", 255, 0, 0 )
+-- Set default screen background color to blue
+display.setDefault( "background", 240, 157, 171 )
 
--- note that the image is saved in a directory ./assests/sprites/
-local image = display.newImageRect( "MyAssets/rots.png", 100, 100 )
-image.x = 160
-image.y = 240
